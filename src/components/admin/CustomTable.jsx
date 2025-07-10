5f7aa86d261326720bf0df92a085be65def521b9
YashwanthSharyo <yashwanth@sharyo.in>
2025-07-10 10:40:57 +0530

    Working on Modifications

--- src/components/admin/CustomTable.jsx
+++ src/components/admin/CustomTable.jsx
@@ -11,99 +11,13 @@ import {
   Paper,
 } from "@mui/material";
 import { apiGet } from "../../api/axios";
-<<<<<<< HEAD:src/components/admin/InstituteTable.jsx
-import CustomButton from "./CustomButton";
-
-const styles = {
-  tableContainer: {
-    boxShadow: 'none',
-    maxHeight: 400,
-    overflowY: 'auto',
-    overflowX: 'auto',
-  },
-  headCell: {
-    fontFamily: 'Jost',
-    fontWeight: 500,
-    fontSize: '15px',
-    color: '#334D6E',
-    textAlign: 'center',
-  },
-  bodyCell: {
-    borderBottom: 'none',
-    fontWeight: 500,
-    fontSize: '14px',
-    color: '#334D6E',
-    textAlign: 'center',
-  },
-  button: {
-    color: 'white',
-    width: 'auto',
-  },
-};
-
-function InstituteTable({ maxWidth }) {
-  const [institutes, setInstitutes] = useState([]);
-
-  const fetchInstitute = async () => {
-    try {
-      const response = await apiGet('/admin/getInstitute');
-      setInstitutes(response.data.data);
-    } catch (error) {
-      console.error('Error fetching institutes:', error);
-    }
-  };
-
-  useEffect(() => {
-    fetchInstitute();
-  }, []);
-=======
 import CustomTypography from "./CustomTypography";
 import CustomButton from "./CustomButton";
 
 function CustomTable({maxWidth, handleClick, institutes, tableHeaders}) {
->>>>>>> 13aa2f4f8731b94ebcd35882efb25d1c181be347:src/components/admin/CustomTable.jsx
 
-  return (
-    <Grid>
-      <TableContainer
-        component={Paper}
-        elevation={0}
-        sx={{ ...styles.tableContainer, maxWidth }}
-      >
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell sx={styles.headCell}>Sr No</TableCell>
-              <TableCell sx={styles.headCell}>Institute Name</TableCell>
-              <TableCell sx={styles.headCell}>Number students</TableCell>
-              <TableCell sx={styles.headCell}>Status</TableCell>
-              <TableCell sx={styles.headCell}>Action</TableCell>
-            </TableRow>
-          </TableHead>
+  
 
-<<<<<<< HEAD:src/components/admin/InstituteTable.jsx
-          <TableBody>
-            {institutes.map((institute, index) => (
-              <TableRow key={index}>
-                <TableCell sx={styles.bodyCell}>{index + 1}</TableCell>
-                <TableCell sx={styles.bodyCell}>{institute.instituteName}</TableCell>
-                <TableCell sx={styles.bodyCell}>1</TableCell>
-                <TableCell sx={styles.bodyCell}>
-                  <CustomButton bgColor="#109CF1" sx={styles.button}>
-                    Active
-                  </CustomButton>
-                </TableCell>
-                <TableCell sx={{ ...styles.bodyCell }}>
-                  <img src="/images/edit.svg" alt="edit" />
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </Grid>
-  );
-=======
     return (
         <>
             <Grid>
@@ -152,7 +66,6 @@ function CustomTable({maxWidth, handleClick, institutes, tableHeaders}) {
 
         </>
     );
->>>>>>> 13aa2f4f8731b94ebcd35882efb25d1c181be347:src/components/admin/CustomTable.jsx
 }
 
 export default CustomTable;
